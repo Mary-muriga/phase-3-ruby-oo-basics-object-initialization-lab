dc5e5a2eaaf4401557f61ea34be91e0b03d32a2a
Marymuriga <marymuriga@gmail.com>
2022-08-21 09:51:58 +0300

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+attr_reader :name, :breed
+
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+        
+    end
+end 
+fido = Dog.new("Fido", "Pug")
+fido.name
+fido.breed
+
+
