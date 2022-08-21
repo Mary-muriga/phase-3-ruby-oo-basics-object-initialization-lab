f7460d13dc3bd39cb3e5ffcc78aecb68c69be2d4
Marymuriga <marymuriga@gmail.com>
2022-08-21 09:47:42 +0300

    commit tree d46abb3e0640e05737b56fe1ad70087f208cc434

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog 
+attr_reader :name, :breed
+    def initialize(name, breed)
+        @name = name
+        @breed = breed
+        
+    end
+end 
+fido = Dog.new("Fido", "Pug")
+fido.name
+fido.breed
+
+
